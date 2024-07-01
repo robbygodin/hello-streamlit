772209bffd9a25b294fe57e0f8fe5974e9d93d63
robbygodin <Rg0413044@gmail.com>
2024-07-01 10:29:36 -0400

    First version with input sections and save button. Saves to Z:

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,16 @@ import pandas as pd
 
 
 st.title('Bulk Loading Log')
-st.text_input('Car Number: ')
-st.text_input('Comp Number: ')
-st.text_input('Blend Number: ')
-st.text_input('Silo Number: ')
-st.text_input('Date/Time Started: ')
-st.text_input('Date/Time End: ')
-st.text_input('Operator Name: ')
-st.text_input('Comments: ')
+carNum = st.text_input('Car Number: ')
+compNum = st.text_input('Comp Number: ')
+blendNum = st.text_input('Blend Number: ')
+siloNum = st.text_input('Silo Number: ')
+dateStart = st.text_input('Date/Time Started: ')
+dateEnd = st.text_input('Date/Time End: ')
+opName = st.text_input('Operator Name: ')
+comments = st.text_input('Comments: ')
+
+if (st.button('Save')):
+    with open('Z:\\Public\\BulkLoadingLog\\Bulk_Loading_Log.csv', 'a', newline='') as fd:
+        fd.writelines(carNum + ',' + compNum + ',' + blendNum + ',' + siloNum + ','
+                    + dateStart + ',' + dateEnd + ',' + opName + ',' + comments + '\n')
